445bd91455175f55bad74b86d8af52e82519f0da
Benjamin Misell <q@misell.cymru>
2018-10-24 14:00:48 +0100

    Change password from admin

--- employees/urls.py
+++ employees/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('view/<id>', views.view_employee, name='view_employee'),
     path('edit/<pk>', views.EmployeeEditView.as_view(), name='edit_employee'),
     path('delete/<pk>', views.EmployeeDeleteView.as_view(), name='delete_employee'),
+    path('password/<pk>', views.employee_change_password, name='employee_change_password'),
     path('add', views.EmployeeCreateView.as_view(), name='add_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse_lazy
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AdminPasswordChangeForm
 
 
 @permission_required("auth.view_user")
@@ -36,23 +38,31 @@ class EmployeeCreateView(LoginRequiredMixin, CreateView):
 
 class EmployeeEditView(LoginRequiredMixin, UpdateView):
     model = User
-    fields = ('username', 'first_name', 'last_name', 'password', 'is_active', 'is_superuser', 'groups', 'user_permissions',
+    fields = ('username', 'first_name', 'last_name', 'is_active', 'is_superuser', 'groups', 'user_permissions',
               'last_login', 'date_joined',)
     success_url = reverse_lazy('employees:index')
     permission_required = ('auth.change_user', )
     template_name = "employees/employee_update_form.html"
 
-    def get_object(self, queryset=None):
-        object = super().get_object(queryset)
-        object.password = ""
-        return object
 
-    def form_valid(self, form):
-        password = form.cleaned_data['password']
-        self.object.set_password(password)
-        self.object.save()
-        form.cleaned_data['password'] = self.object.password
-        return super().form_valid(form)
+@permission_required("auth.change_user")
+def employee_change_password(request, pk):
+    user = get_object_or_404(User, id=pk)
+    if request.method == 'POST':
+        form = AdminPasswordChangeForm(user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = AdminPasswordChangeForm(user)
+    return render(request, 'employees/employee_change_password.html', {
+        'form': form,
+        'object': user,
+    })
 
 
 class EmployeeDeleteView(LoginRequiredMixin, DeleteView):
